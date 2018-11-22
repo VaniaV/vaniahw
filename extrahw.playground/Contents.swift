419e3c5176d2e81e7cf5cc6d06d605a65a86eed9
Vania Vergun <ivanvergunv2@i.ua>
2018-11-22 12:30:32 +0200

    Test commit

---
+++ extrahw.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Foundation
+
+func calculate(){
+    var year: Int
+    var month: Int
+    var day: Int
+    var hour: Int
+    var minute: Int
+    var second: Int
+    var date: String{
+        return "\(day).\(month).\(year)"
+    }
+    
+}
